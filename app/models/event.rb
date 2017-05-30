57bd3c6788184d141023abedb4f09da190cbcb22
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-30 09:33:57 -0400

    Set mandatory fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   has_many :dialogues
   has_many :teachers, through: :dialogues
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :convener, :presence => true
+  validates :start_datetime, :presence => true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,6 @@
 class Teacher < ApplicationRecord
   has_many :dialogues
   has_many :events, through: :dialogues
+  validates :name, :presence => true
+  validates :email, :presence => true
 end
